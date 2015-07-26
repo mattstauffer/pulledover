433b091022d084d57b97b875557cfff83c4cf595
Matt Stauffer <matt@tighten.co>
2015-07-26 02:24:53 -0400

    Intro landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <title>Pulled Over</title>
 
         <style>
             html, body {
@@ -11,34 +9,38 @@
             }
 
             body {
+                font-family: arial;
+                font-size: 20px;
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
             }
 
             .container {
+                margin: 0 auto;
+                max-width: 40rem;
                 text-align: center;
-                display: table-cell;
-                vertical-align: middle;
             }
 
             .content {
+                margin-top: 5rem;
                 text-align: center;
-                display: inline-block;
             }
 
             .title {
-                font-size: 96px;
+                font-size: 4rem;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Pulled Over</div>
+                <p>The goal: Give you a free app that is simple to set up and allows you to turn it on when you get pulled over, and it'll notify your friends or family, and start recording, and hopefully also capture geographic coordinates.</p>
+                <p>How it works right now: It will like be a phone app later. Right now, it's just a phone number. The goal is to let you set up your contacts but for the current version, it just texts you a link back with a recording of everything that happened. Not all that useful, I know.</p>
+                <p>Call now: <a href="tel:+18443116837">1-844-311-OVER</a> (<a href="tel:+18443116837">1-844-311-6837</a>)</p>
+                <br><hr><br>
+                Built by <a href="http://mattstauffer.co/">Matt Stauffer</a>, powered by <a href="http://twilio.com/">Twilio</a> and <a href="http://laravel.com/">Laravel</a>, source on <a href="http://github.com/mattstauffer/pulledover">GitHub</a>
             </div>
         </div>
     </body>
