91898abc8fe1d159230884725e56e41e6125b27c
Matt Stauffer <matt@tighten.co>
2015-07-26 02:18:49 -0400

    Clean up for first functioanl version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,7 @@ Route::post('call', function () {
     $response = new Services_Twilio_Twiml();
     $response->say('Thank you for calling Pulled Over. Your audio is now being recorded.');
     $response->record([
-        // 'maxLength' => 120,
+        'maxLength' => 120,
         'action' => '/after-call',
     ]);
 
@@ -19,9 +19,6 @@ Route::post('call', function () {
 
 Route::post('after-call', function (Illuminate\Http\Request $request) {
     // Grab recording and text it to someone
-    // @todo https://www.twilio.com/blog/2015/07/record-a-phone-call-in-ruby.html
-
-    // Testing
     $sid = env('TWILIO_ACCOUNT_SID');
     $token = env('TWILIO_ACCESS_TOKEN');
 
@@ -77,38 +74,4 @@ Route::post('after-call', function (Illuminate\Http\Request $request) {
  * [CalledCity] =>
  * [RecordingSid] => Long hex
  */
-
-    // https://github.com/thatpodcast/voicemail/blob/master/app.php
-    // $email->addTo($to)
-    //     ->setFrom($to)
-    //     ->setSubject("New Voicemail!")
-    //     ->setText(sprintf(
-    //         "SID: %s\nDuration: %s\nURL: %s\n",
-    //         $request->get("CallSid"),
-    //         $request->get("RecordingDuration"),
-    //         $request->get("RecordingUrl")
-    //     ));
-    // $rsp = $app['sendgrid']->send($email);
-    // if (is_object($rsp) && isset($rsp->errors)) {
-    //     $app['logger']->error(json_encode($rsp));
-    // }
-    // $twiml = new Twiml;
-    // $twiml->say("Thanks for leaving a message, you're awesome.");
-    // $twiml->hangup();
-    // return new Response((string) $twiml);
 });
-
-// Route::get('text-my-friend', function () {
-//     // test, of course this will happen in response to call
-//     $sid = env('TWILIO_ACCOUNT_SID');
-//     $token = env('TWILIO_ACCESS_TOKEN');
-
-//     $client = new Services_Twilio($sid, $token);
-//     $message = $client->account->messages->sendMessage(
-//         env('TWILIO_FROM_NUMBER'), // From a valid Twilio number
-//         '1234567890', // Text this number
-//         "Testing I am being pulled over!!"
-//     );
-
-//     print $message->sid;
-// });
