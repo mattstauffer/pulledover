9c5d0265e013a83897bb73fccbc8754b4e22339b
Matt Stauffer <matt@tighten.co>
2015-07-25 21:36:47 -0400

    Include twilio

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Aloha\Twilio\Support\Laravel\ServiceProvider::class,
     ],
 
     /*
@@ -191,7 +191,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Twilio'    => Aloha\Twilio\Support\Laravel\Facade::class,
     ],
 
 ];
