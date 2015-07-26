a104788e393469f2426205cb83fd5f8b9604b746
Matt Stauffer <matt@tighten.co>
2015-07-26 02:00:42 -0400

    Record call

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,21 +8,60 @@ Route::post('call', function () {
     // When someone calls our number, say hello and play 5 cowbells
     $response = new Services_Twilio_Twiml();
     $response->say('Thank you for calling Pulled Over. Your audio is now being recorded.');
-    $response->play('https://api.twilio.com/cowbell.mp3', ["loop" => 5]);
+    $twiml->record([
+        // 'maxLength' => 120,
+        'action' => '/after-call',
+    ]);
     print $response;
 });
 
-Route::get('text-my-friend', function () {
-    // test, of course this will happen in response to call
-    $sid = env('TWILIO_ACCOUNT_SID');
-    $token = env('TWILIO_ACCESS_TOKEN');
-
-    $client = new Services_Twilio($sid, $token);
-    $message = $client->account->messages->sendMessage(
-        env('TWILIO_FROM_NUMBER'), // From a valid Twilio number
-        '1234567890', // Text this number
-        "Testing I am being pulled over!!"
-    );
-
-    print $message->sid;
+Route::get('table', function (Illuminate\Http\Request $request) {
+    Log::info(print_r($request, true));
 });
+
+Route::post('after-call', function () {
+    // Grab recording and text it to someone
+    // @todo https://www.twilio.com/blog/2015/07/record-a-phone-call-in-ruby.html
+
+    Log::info(print_r($request));
+
+    $response = new Services_Twilio_Twiml();
+    $response->say('If you are hearing this, our application has run out of space or something and is hanging up.');
+    $response->hangup();
+
+    print $twiml;
+
+    // https://github.com/thatpodcast/voicemail/blob/master/app.php
+    // $email->addTo($to)
+    //     ->setFrom($to)
+    //     ->setSubject("New Voicemail!")
+    //     ->setText(sprintf(
+    //         "SID: %s\nDuration: %s\nURL: %s\n",
+    //         $request->get("CallSid"),
+    //         $request->get("RecordingDuration"),
+    //         $request->get("RecordingUrl")
+    //     ));
+    // $rsp = $app['sendgrid']->send($email);
+    // if (is_object($rsp) && isset($rsp->errors)) {
+    //     $app['logger']->error(json_encode($rsp));
+    // }
+    // $twiml = new Twiml;
+    // $twiml->say("Thanks for leaving a message, you're awesome.");
+    // $twiml->hangup();
+    // return new Response((string) $twiml);
+});
+
+// Route::get('text-my-friend', function () {
+//     // test, of course this will happen in response to call
+//     $sid = env('TWILIO_ACCOUNT_SID');
+//     $token = env('TWILIO_ACCESS_TOKEN');
+
+//     $client = new Services_Twilio($sid, $token);
+//     $message = $client->account->messages->sendMessage(
+//         env('TWILIO_FROM_NUMBER'), // From a valid Twilio number
+//         '1234567890', // Text this number
+//         "Testing I am being pulled over!!"
+//     );
+
+//     print $message->sid;
+// });
