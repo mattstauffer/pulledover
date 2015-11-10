2bb3b5bc5c4d9c4f7ce20f614a105254c3baefa7
Matt Stauffer <matt@tighten.co>
2015-11-10 00:34:42 -0500

    Real layout on recordings page

--- resources/views/recordings/index.blade.php
+++ resources/views/recordings/index.blade.php
@@ -11,16 +11,22 @@
                         @if ($i === 0)
                         <thead>
                             <tr>
-                                @foreach ($recording->toArray() as $col => $b)
-                                    <th>{{ $col }}</th>
-                                @endforeach
+                                <th>From</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Recording</th>
+                                <th>Duration</th>
+                                <th>Date/Time</th>
                             </tr>
                         </thead>
                         @endif
                         <tr>
-                            @foreach ($recording->toArray() as $field)
-                            <td>{{ $field }}</td>
-                            @endforeach
+                            <th>{{ $recording->from }}</th>
+                            <th>{{ $recording->city }}</th>
+                            <th>{{ $recording->state }}</th>
+                            <th><a href="{{ $recording->url }}">{{ $recording->url }}</a></th>
+                            <th>{{ $recording->duration }}s</th>
+                            <th>{{ $recording->created_at }}</th>
                         </tr>
                     @empty
                         <tr>
