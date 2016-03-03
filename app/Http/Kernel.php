1ecb974c047a23201ef8a6e22c340b6de9ef5ca4
Laravel Shift <development+laravel.shift@pureconcepts.net>
2016-03-03 10:31:01 -0500

    Shift Middleware
    Laravel 5.2 adjusts the `Guard` object used within middleware. In
    addition, a new `throttles` middleware was added.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\Admin::class,
     ];
 }
