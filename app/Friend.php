632a04522949b3046007e4c06b2196cf5bbc58f4
Ricky Delorier <rdelorier@gmail.com>
2016-06-05 16:21:52 -0400

    Remove redundant attributes from Friend

--- app/Friend.php
+++ app/Friend.php
@@ -6,14 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Friend extends PhoneNumber
 {
-    use formatsNumber;
-
     protected $fillable = [
         'name',
         'number'
-    ];
-
-    protected $casts = [
-        'is_verified' => 'boolean'
     ];
 }
