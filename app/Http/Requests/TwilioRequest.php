5f48d54a70255f493cf8bed23b49d8991e1830b1
Ricky Delorier <rdelorier@gmail.com>
2016-07-08 22:44:26 -0400

    Cleanup twilio request

--- app/Http/Requests/TwilioRequest.php
+++ app/Http/Requests/TwilioRequest.php
@@ -11,17 +11,18 @@ use Services_Twilio_RequestValidator;
 class TwilioRequest extends Request
 {
     /**
+     * @var PhoneNumber The model instance retrieved using the From number.
+     */
+    private $phoneNumber = null;
+
+    /**
      * Validate that the incoming request is from twilio.
      *
      * @return bool
      */
     public function authorize(Services_Twilio_RequestValidator $validator)
     {
-        if (app()->isLocal()) {
-            return true;
-        }
-
-        return $validator->validate(
+        return app()->isLocal() || $validator->validate(
             $this->headers->get('X-Twilio-Signature'),
             $this->fullUrl(),
             $this->input()
@@ -35,17 +36,18 @@ class TwilioRequest extends Request
 
     public function phoneNumber()
     {
-        $number = PhoneNumber::byNumber($this->input('From'))->first();
+        if (is_null($this->phoneNumber)) {
+            //set the number to first [PhoneNumber | Friend | NULL]
+            $this->phoneNumber = PhoneNumber::byNumber($this->input('From'))->first()
+                ?: Friend::byNumber($this->input('From'))->first()
+                ?: false;
+        }
 
-        return $number ?: Friend::byNumber($this->input('From'))->first();
+        return $this->phoneNumber;
     }
 
     public function isFromVerifiedNumber()
     {
-        if ($number = $this->phoneNumber()) {
-            return $number->is_verified;
-        }
-
-        return false;
+        return $this->phoneNumber() && $this->phoneNumber()->is_verified;
     }
 }
